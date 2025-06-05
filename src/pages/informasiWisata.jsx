f5691341cc0b45262a2f4a603fe588d7aedca818
Alvien Yuzach Fa'awa <145243962+alvincomot@users.noreply.github.com>
2025-06-05 22:02:00 +0700

    Update informasiWisata.jsx

--- src/pages/informasiWisata.jsx
+++ src/pages/informasiWisata.jsx
@@ -11,7 +11,7 @@ function InformasiWisata() {
           <section className="max-w-6xl overflow-hidden bg-gray-100 sm:grid sm:grid-cols-2 py-10 px-4 sm:px-6 lg:px-8">
             <img
               alt=""
-              src="/footage1.jpg"
+              src="/footage1.png"
               className="h-56 w-full object-cover sm:h-full"
             />
 
@@ -45,7 +45,7 @@ function InformasiWisata() {
           <section className="max-w-6xl overflow-hidden bg-gray-100 sm:grid sm:grid-cols-2 py-10 px-4 sm:px-6 lg:px-8">
             <img
               alt=""
-              src="/footage1.jpg"
+              src="/footage1.png"
               className="h-56 w-full object-cover sm:h-full"
             />
 
