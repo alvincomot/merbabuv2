a6b21e0faf85b45d3ab13ae7d904b95036fa865d
alvincomot <limithunter1@gmail.com>
2025-08-08 20:01:29 +0700

    fix deployment

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:3000',
+    baseURL: 'mysql://uc84hzvxlvdf1wqt:Dn9zpoGWHXJG5f3xDMIY@bvtdrb6rxsnlusu0bkb6-mysql.services.clever-cloud.com:3306/bvtdrb6rxsnlusu0bkb6',
     withCredentials: true
 });
 
