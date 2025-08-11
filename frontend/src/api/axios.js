68d53319702af0849ce36ebf4bfed93b5a38027a
alvincomot <limithunter1@gmail.com>
2025-08-11 16:34:14 +0700

    fix error

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: "https://merbabu-backend.up.railway.app/api", 
+  baseURL: "https://merbabu-backend.up.railway.app/api",
   withCredentials: true
 });
 
