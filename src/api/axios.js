c0774862152516c5703f0f50d8dcc6956fb583d9
alvincomot <limithunter1@gmail.com>
2025-08-15 20:50:32 +0700

    fix deployment, bismillah

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,5 +1,8 @@
-import axios from "axios";
-export default axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "/api",
-  withCredentials: true,
+import axios from 'axios';
+
+const api = axios.create({
+  baseURL: `${import.meta.env.VITE_API_URL}/api`,
+  withCredentials: true
 });
+
+export default api;
